61cde5fc9187a4e555999d15a8fb1efc2c561f79
amie543 <54036834+amie543@users.noreply.github.com>
2022-12-08 21:57:05 +0630

    Release

--- src/App.js
+++ src/App.js
@@ -7,27 +7,50 @@ const App = () => {
   const [name, setName] = useState('');
   const [price, setPrice] = useState(0);
   const [isEdit, setIsEdit] = useState(0);
-   
+  const [errMsg, setErrMsg] = useState('');
   useEffect(() => {
       // console.log("UPName: ",updateName);
       // console.log("UpPrice ",updatePrice);
       // console.log(product);
   });  
   const add = useCallback(() => {
-    setProduct([...product, { id:product.length+1,name, price }]);    
-    setName("");
-    setPrice(0);
+    if (name && price) {
+      console.log("True");
+      setProduct([...product, { id:product.length+1,name, price }]);         
+      setName("");
+      setPrice(0);;
+    } else {
+      console.log("False");
+      setErrMsg(
+        !name ? "Please Insert Name" :
+          !price ? "Please Insert Price" :
+            "Error"
+      );
+      autoCloseErrMsg();
+    }    
   }, [name, price, product]);   
+  const autoCloseErrMsg = async () => {
+    console.log("Before");
+    await sleep(3000);
+    console.log("After");
+    setErrMsg("");    
+  }
   const remove = useCallback(({ id }) => {
+    if (isEdit) {
+      setErrMsg(" Can not delete items while you edit");
+      autoCloseErrMsg();
+      return;
+    }
     const newProduct = product.filter(p => p.id !== id);
-      setProduct(newProduct);
-    // console.log("Delete ",id);    
-  }, [product]);
-  const initEdit = useCallback(({id,name,price}) => {
+    setProduct(newProduct);
+    // console.log("Delete ",id);     
+  }, [product,isEdit]);
+  const initEdit = useCallback(({ id, name, price }) => {
+    console.log(id);
     setIsEdit(id);
     setName(name);
     setPrice(price);    
-  }, []);
+  }, [isEdit,name,price]);
   const editProduct = useCallback(async() => {
     const newProduct = product.map(p => {
       if (p.id === isEdit) {
@@ -44,27 +67,43 @@ const App = () => {
     setProduct(newProduct);   
     //reset();
   }, [product, name, price, isEdit]);
-  // const sleep = ms => new Promise( _=>setInterval(_,ms) );
+  const sleep = ms => new Promise( _=>setInterval(_,ms) );
   // const reset = async () => {   
   //   await sleep(1000);    
   // }
   return (
-    <div className="flex flex-row container mx-auto justify-evenly">     
-      <div className="flex-col items-center mt-16">
+    <div className="flex container mx-auto justify-evenly">     
+      <div className="flex-col items-center mt-16 justify-center">
+        <div className="text-2xl text-center font-semibold ">React Hook Tutorial</div>
+        <div className="text-lg text-center font-semibold italic text-blue-500 pt-4 pb-8 ">Trained By Arkar Mann Aung</div>
         <Input name="Product Name" setValue={setName} value={name} placeholder="Enter Product Name"/>
         <Input name="Product Price" setValue={setPrice} value={price} placeholder="Enter Product Price" /> 
         <Button btnState={isEdit} editProductFunc={editProduct} addProductFunc={add} />
+        <div className="text-red-600 mt-5 italic font-bold animate-pulse">{ errMsg}</div>
         <p className="font-bold text-center text-lg py-5">Product List</p>
         { 
-        product.map((p, i) => {
-     
+        product.map((p, i) => {     
             return (              
-              <Product btnState={isEdit} remove={remove} edit={initEdit} product={p} name={p.name} price={p.price} key={i} id={p.id} />           
+              <Product
+                btnState={isEdit}
+                remove={remove}
+                edit={initEdit}
+                product={p}
+                name={p.name}
+                price={p.price}
+                index={i}
+                id={p.id}
+                length={product.length}
+                key={i} />           
             );            
         })        
         }    
+        <div className="mt-5">
+          Source Code 👉<a href="https://github.com/amemelwin/React-Testing.git" className="text-blue-500 ml-5  underline italic" >GitHub </a>       
+        </div>
+        <div className="text-sm">This Code will not be covered for React Hook if you don't have the knowledge in React</div>
+        <div className="text-lg text-center font-semibold italic text-pink-500 pt-4 pb-8  flex justify-end">By Amie</div>        
       </div>
-       
     </div>
     )  
 };
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,12 @@
 import { memo } from "react";
-const Product = ({name,price,remove,edit,product,btnState,id}) => {
+const Product = ({name,price,remove,edit,product,btnState,index,length}) => {
     return (
-        <div className={`${product.id %2 === 0 ?"bg-pink-400":"bg-sky-400"} flex flex-row justify-center text-yellow-50 ${btnState === product.id?"opacity-25":"" }`}>       
+        <div className={`${product.id %2 === 0 ?"bg-blue-300":"bg-sky-400"} flex flex-row  pt-5 h-16 text-yellow-50 ${btnState === product.id?"opacity-50":"" } ${index === 0?"rounded-t-2xl":"" } ${index ===length-1?"rounded-b-2xl":""}`}>       
         {/* <div className={`${btnState === product.id?"opacity-25":""} flex flex-row justify-center`}>        */}
-                <h1 className="px-5">{name}</h1>
-                <h1>{price}</h1>
-                <div  onClick={()=>edit(product) } className="px-2 cursor-pointer text-blue-600 font-bold">edit</div>
-                <div onClick={()=>remove(product)} className="px-2 cursor-pointer text-red-600 font-extrabold">X</div>
+            <h1 className="px-5 flex flex-1">{name}</h1>
+            <h1 className="flex flex-1">{price}</h1>
+            <div onClick={() => edit(product)} className={`px-2 cursor-pointer text-blue-600 font-bold `}>edit</div>
+            <div onClick={()=>remove(product)} className="mr-5 text-lg cursor-pointer text-red-600 font-extrabold ">X</div>                
         </div>        
     );
     
