d29339e50a17b371734fd8129fa617f712f2f1b0
amie543 <54036834+amie543@users.noreply.github.com>
2022-12-11 19:47:47 +0630

    Pull

--- src/screens/AddProductQtyScreen.js
+++ src/screens/AddProductQtyScreen.js
@@ -2,14 +2,9 @@ import { memo, useEffect } from "react";
 import { useLocation } from "react-router-dom"; 
 
 const AddProductQtyScreen = ()=>{
-    const location = useLocation();
-    // let name = location.state.name;
-    // useEffect(()=>{
-    //     console.log(location.state.name);
-    // })
+    const location = useLocation();    
     return (
-        <div>
-            Hello World 
+        <div>           
             {location.state.name}
         </div>
     );
